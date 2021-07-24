6df607ab56adc21104152bf0953b80859455fe3c
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-07-24 17:41:33 -0400

    collection preview filtered to 4 items

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SHOP_DATA from './shop.data';
+import PreviewCollection from '../../components/preview-collection/preview-collection.component';
 
 
 
@@ -14,9 +15,16 @@ class ShopPage extends React.Component {
         }
     }
 
-    render(){
-        return(
-            <div>SHOPPAGE</div>
+    render() {
+        const { collections } = this.state;
+        return (
+            <div className='shop-page'>
+                {
+                    collections.map(({ id, ...otherCollectionProps }) => (
+                        <PreviewCollection key={id} {...otherCollectionProps} />
+                    ))
+                }
+            </div>
         )
     }
 }
