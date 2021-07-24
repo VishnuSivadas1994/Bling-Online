5b598924fbf8420253b26462f37b79b5d208847c
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-07-24 17:21:47 -0400

    collection previews added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 
-const HatsPage=()=>(
+const HatsPage = () => (
   <div><h1>HATSPAGEEE</h1></div>
 )
 
@@ -12,10 +13,10 @@ const HatsPage=()=>(
 function App() {
   return (
     <div >
-    <Switch>
-      <Route exact path='/' component={Homepage} />
-      <Route path='/shop/hats' component={HatsPage} />
-    </Switch>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data';
+
+
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+
+        }
+    }
+
+    render(){
+        return(
+            <div>SHOPPAGE</div>
+        )
+    }
+}
+
+export default ShopPage;
