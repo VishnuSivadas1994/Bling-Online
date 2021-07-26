03e833fe256d93b97bbddf0fc267a4d4bf498034
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-07-25 23:17:56 -0400

    Google OAuth set up

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,7 +5,7 @@ import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -19,7 +19,12 @@ const Header = ({currentUser}) => (
             <Link className='option' to='/shop'>
                 CONTACT
             </Link>
-
+            {
+                currentUser ?
+                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                    :
+                    <Link className='option' to='/signin'>SIGN IN</Link>
+            }
         </div>
 
     </div>
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -41,9 +41,11 @@ class SignIn extends React.Component {
                     <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="email" required />
 
                     <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required />
+                    <div className='buttons'>
+                        <CustomButton type="submit"> Sign In </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn >Sign in with Google</CustomButton>
 
-                    <CustomButton type="submit"> Sign In </CustomButton>
-                    <CustomButton onClick={signInWithGoogle} > Sign in with Google </CustomButton>
+                    </div>
 
                 </form>
 
