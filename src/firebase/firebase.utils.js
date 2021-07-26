82b57fa11388401c820c22426789d62ef050ff94
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-07-25 22:15:40 -0400

    firebase config

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+
+const config = {
+    apiKey: "AIzaSyBMIL0PbXITJOkuq17iWRAqw0xt8tmwDHs",
+    authDomain: "bling-db-15df0.firebaseapp.com",
+    projectId: "bling-db-15df0",
+    storageBucket: "bling-db-15df0.appspot.com",
+    messagingSenderId: "675393395021",
+    appId: "1:675393395021:web:beb8407bc97d6ff2d0ff04",
+    measurementId: "G-G0PGTT7LFC"
+}
