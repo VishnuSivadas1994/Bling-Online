e4185a2a8a015c2080bf08eb8d859fc9e2ed9091
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-07-29 18:49:10 -0400

    cart init

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import { connect } from 'react-redux';
 
@@ -27,6 +28,8 @@ const Header = ({ currentUser }) => (
                     :
                     <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon/>
+
         </div>
 
     </div>
